3574f5a3e8d185b0db00580b1b4647c65af2df4e
tainafernandes <tainafefe@gmail.com>
2022-10-02 20:04:57 -0300

    Entity Course and Registration

---
+++ src/main/java/com/io/github/tainafernandes/SchoolProject/entity/Registration.java
@@ -0,0 +1,8 @@
+package com.io.github.tainafernandes.SchoolProject.entity;
+
+import javax.persistence.Table;
+
+@Table(name = "tb_registration")
+public class Registration {
+    private Boolean graduated;
+}
