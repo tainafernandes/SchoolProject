7400f47da3124a2aef47e2465d10dd259483ff7e
tainafernandes <tainafefe@gmail.com>
2022-10-02 19:56:25 -0300

    Entity Student

---
+++ src/main/java/com/io/github/tainafernandes/SchoolProject/entity/Student.java
@@ -0,0 +1,22 @@
+package com.io.github.tainafernandes.SchoolProject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String registry;
+    private String name;
+    private String cpf;
+    private String email;
+}
