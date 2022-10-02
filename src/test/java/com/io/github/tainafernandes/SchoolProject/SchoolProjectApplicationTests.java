25863c433a5a3123ce57f015302937a894119bdd
tainafernandes <tainafefe@gmail.com>
2022-10-02 19:36:37 -0300

    First commit

---
+++ src/test/java/com/io/github/tainafernandes/SchoolProject/SchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.tainafernandes.SchoolProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ target/classes/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_students (name, note1, note2) VALUES ('Tainá', 10, 7);
+INSERT INTO tb_students (name, note1, note2) VALUES ('Vinicius', 5, 7);
