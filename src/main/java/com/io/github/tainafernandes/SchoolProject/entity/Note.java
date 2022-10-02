835de5ce952bfa371fa1bdb87083cd3394bf6dfe
tainafernandes <tainafefe@gmail.com>
2022-10-02 19:51:45 -0300

    Entity Note

---
+++ src/main/java/com/io/github/tainafernandes/SchoolProject/entity/Note.java
@@ -0,0 +1,19 @@
+package com.io.github.tainafernandes.SchoolProject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_note")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double note;
+}
