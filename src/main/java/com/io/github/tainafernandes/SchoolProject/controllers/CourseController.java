2a645182d3ac076e6efa4f6dac2463668ec758f2
Tainá Fernandes <tainafefe@gmail.com>
2022-10-26 18:16:15 -0300

    Create HTTP methods

--- src/main/java/com/io/github/tainafernandes/SchoolProject/controllers/CourseController.java
+++ src/main/java/com/io/github/tainafernandes/SchoolProject/controllers/CourseController.java
@@ -1,13 +1,15 @@
 package com.io.github.tainafernandes.SchoolProject.controllers;
 
+import com.io.github.tainafernandes.SchoolProject.dto.CourseDto;
+import com.io.github.tainafernandes.SchoolProject.dto.CreateStudentDto;
+import com.io.github.tainafernandes.SchoolProject.dto.UpdateStudentDto;
 import com.io.github.tainafernandes.SchoolProject.entity.Course;
 import com.io.github.tainafernandes.SchoolProject.services.CourseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +28,21 @@ public class CourseController {
     @GetMapping
     public List<Course> findAll(){
         return  service.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody @Valid CourseDto courseDto){
+        return ResponseEntity.ok(service.create(courseDto));
+    }
+
+    @PutMapping(value = "{id}")
+    public ResponseEntity update (@RequestBody @Valid CourseDto courseDto){
+        return ResponseEntity.ok(service.update(courseDto));
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/io/github/tainafernandes/SchoolProject/services/CourseService.java
+++ src/main/java/com/io/github/tainafernandes/SchoolProject/services/CourseService.java
@@ -1,10 +1,12 @@
 package com.io.github.tainafernandes.SchoolProject.services;
 
+import com.io.github.tainafernandes.SchoolProject.dto.CourseDto;
 import com.io.github.tainafernandes.SchoolProject.entity.Course;
-import com.io.github.tainafernandes.SchoolProject.entity.Student;
 import com.io.github.tainafernandes.SchoolProject.repositories.CourseRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +23,24 @@ public class CourseService {
 
     public List<Course> findAll(){
         return repository.findAll();
+    }
+
+    public Object create(CourseDto courseDto) {
+        Course course = new Course();
+        course.setName(courseDto.getName());
+        return repository.save(course);
+    }
+
+    public Object update(CourseDto courseDto) {
+        Course course = new Course();
+        course.setName(courseDto.getName());
+        return repository.save(course);
+    }
+
+
+    public void delete(Long id) {
+        Optional<Course> courseOptional = repository.findById(id);
+        courseOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course does not exist"));
+        repository.deleteById(id);
     }
 }
